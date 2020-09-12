0ec81106d8689d8ce52f325463caece095e7d023
ShreeP2020 <shreeyogeshpatil2020@gmail.com>
2020-09-12 22:31:58 +0200

    Initial commit

---
+++ Python - number data type/math_functions.py
@@ -0,0 +1,13 @@
+import math
+
+loss = -20.25
+product_cost = 89.12
+
+print(abs(loss))
+print(math.floor(product_cost))
+print(math.ceil(product_cost))
+print(abs(math.floor(loss)))
+print(round(product_cost))
+print(math.sqrt(product_cost))
+print(math.pow(5, 3))
+print(5 ** 3)
