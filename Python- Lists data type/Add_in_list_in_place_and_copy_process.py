24dee03355c7e5a849ed92b5117207d6317c290f
ShreeP2020 <shreeyogeshpatil2020@gmail.com>
2020-09-16 22:42:27 +0200

    Initial commit

---
+++ Python- Lists data type/Add_in_list_in_place_and_copy_process.py
@@ -0,0 +1,17 @@
+tags = ['python', 'development', 'tutorials', 'code']
+
+#Nope
+#tags[-1] = 'programming'
+
+# In Place
+#tags.extend('programming')
+
+tags.extend(['programming'])
+
+print(tags)
+
+# New List
+new_tags = tags + ['guides']
+
+print(tags)
+print(new_tags)
---
+++ Python- Lists data type/sorted_function_in_lists.py
@@ -0,0 +1,23 @@
+sale_prices = [
+  100,
+  83,
+  220,
+  40,
+  100,
+  400,
+  10,
+  1,
+  3
+]
+
+sale_prices.sort()
+
+print(sale_prices)
+
+sorted_list = sorted(sale_prices)
+
+print(sorted_list)
+
+sorted_list = sorted(sale_prices, reverse=True)
+
+print(sorted_list)
