fd340bfa796840e0fb98594e50333ce32c8afd6f
ShreeP2020 <shreeyogeshpatil2020@gmail.com>
2020-09-16 18:38:51 +0200

    Initial commit

---
+++ Python- Lists data type/Ranges_in_lists.py
@@ -0,0 +1,16 @@
+tags = ['python', 'development', 'tutorials', 'code']
+
+tag_range = tags[2:]
+print(tag_range)
+
+tag_range = tags[0:2]
+print(tag_range)
+
+tag_range = tags[:2]
+print(tag_range)
+
+tag_range = tags[:]
+print(tag_range)
+
+tag_range = tags[0:-1]
+print(tag_range)
---
+++ Python- Lists data type/implementing_ranges_and_slices_in_lists.py
@@ -0,0 +1,22 @@
+tags = [
+    'python',
+    'development',
+    'tutorials',
+    'code',
+    'programming',
+    'computer science'
+]
+
+tag_range = tags[:-1:2]
+
+print(tag_range)
+
+#slicing in lists
+tag_range = tags[::-1]
+
+print(tag_range)
+
+#sorting in list
+tags.sort(reverse=True)
+
+print(tags)
